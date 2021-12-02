8234b0bf0a6e3af923f1012ce88651e90550765b
Emmanuel Torres <torresvazquez_emmanuel@outlook.com>
2021-12-02 08:51:57 -0600

    Clases, Objetos y metodos

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        Car car = new Car("TPYHt123", new Account("Emmanuel Torres", "EMM123"));
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car("TPYHtA3", new Account("Eloisa Cruz", "ELO123"));
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,9 @@
+from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    
+    car = Car("SDFR34", Account("Emmanuel Torres", "ASF35"))
+    print(vars(car))
+    print(vars(car.driver))
